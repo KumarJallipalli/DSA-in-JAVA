0234267d6179858f154eb215e199066e176f8116
Siva Jallipalli <kumar.jallipalli@gmail.com>
2023-08-16 23:59:52 +0530

    Added Intro session from Beginner's Class

---
+++ Beginner_Classes/Intro.java
@@ -0,0 +1,28 @@
+package Beginner_Classes;
+
+public class Intro {
+    public static void main(String[] args) {
+        /*
+         * INTRO:
+         * Computers are Dumb/stupid, but are very quick.
+         * i.e., why we need to elaborate each & every step to it which it needs to be performed, so that it can perform that task in an un-imaginably faster way.
+        */
+
+        /*
+         * WKT, switches has 2 states, ON & OFF.
+         * Computers has billions of those switches called transistors. That means computer can only understand ON & OFF states of these switches. These are called 0 & 1 in simpler binary terms.
+         * So, We need to give all the steps [instructions] which the computer needs to be performed in these combination of 1's & 0's.
+         * The set of instructions given to a computer to perform a specific task is called "Program"
+         * As these set of instructions are given/written in combinations of 1's & 0's, This is termed as Machine language or Binary language
+        */
+
+        /*
+         * JAVA is a High Level, Object Oriented, Statistically/Loosely typed programming Language
+         * Programming language is the language in whihc the programs are written.
+         * 
+         * Then, How does the computer understand this java language..?
+         * Ans -> It doesn't. There will be a software called "Compiler" which translate the Java's HLL to binary language, which is understandable by computer.
+         *      Compiler will also check the syntax errors. As these errors are found during compilation, theya re called "compilation errors"
+        */
+    }
+}
