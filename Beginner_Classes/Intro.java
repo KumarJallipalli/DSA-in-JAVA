a2f7797505b041f0d6fa303667a066501e8cec3f
Siva Jallipalli <kumar.jallipalli@gmail.com>
2023-08-17 23:39:15 +0530

    Added partial changes to Intro.class file

--- Beginner_Classes/Intro.java
+++ Beginner_Classes/Intro.java
@@ -24,5 +24,34 @@ public class Intro {
          * Ans -> It doesn't. There will be a software called "Compiler" which translate the Java's HLL to binary language, which is understandable by computer.
          *      Compiler will also check the syntax errors. As these errors are found during compilation, theya re called "compilation errors"
         */
+
+        /*
+         * Comments -> are the like rough work in main notes
+         *      Comments are ignored by the compiler
+         *      Comments are used for documentation, code readability & easy understanding
+         *      Comments are sometimes used for debugging the code
+        */
+
+        /*
+         * HOW to take input in JAVA..?
+         * Scanner class is used to take user inputs in JAVA
+         * Scanner is used for taking smaller data as input. While BufferReader is used to take larger size data [like 1000's lines of code] as inputs.
+         * But to use Scanner class in java, some steps needs to be followed
+         *      1.) Import the Scanner class from java.util library
+         *      2.) Build the Scanner & define the Scanner object 
+         *      3.) Take the user input using Scanner object.
+        */
+
+        /*
+         * HOW to display the output in JAVA..?
+         * System class is used to display the output on the console.
+         *      System.out.print() -> prints the o/p & cursor will be positioned at the end of o/p
+         *      SYstem.out.println() -> prints the output * cursor will be positioned at the start of new line
+        */
+
+        //" + " operator will execute the code from Left to Right
+        System.out.println(10 + 20 + "Hello");
+        System.out.println("Hello" + 10 + 20); 
+
     }
 }
